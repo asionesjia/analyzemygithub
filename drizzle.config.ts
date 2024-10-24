e014f77dfbdc4d028d1eb5f6eee2a1c4fb017d74
John Smith <asionesjia@gmail.com>
2024-10-24 20:35:14 +0800

    :tada: Start Analyzemygithub!

---
+++ drizzle.config.ts
@@ -0,0 +1,10 @@
+import { type Config } from 'drizzle-kit'
+import { envServer } from '~/env/server'
+
+export default {
+  schema: './src/server/db/schema.ts',
+  dialect: 'postgresql',
+  dbCredentials: {
+    url: envServer.DATABASE_URL,
+  },
+} satisfies Config
