a335ae796613346deb2a93a9bde7bff1eb7853fe
John Smith <asionesjia@gmail.com>
2024-10-25 02:45:04 +0800

    :wrench: 更新env

--- src/env/server.ts
+++ src/env/server.ts
@@ -1,23 +1,22 @@
-import { createEnv } from "@t3-oss/env-nextjs";
+import { createEnv } from '@t3-oss/env-nextjs'
 import { z } from 'zod'
 
 export const envServer = createEnv({
   server: {
     DATABASE_URL: z.string().url(),
-    NODE_ENV: z
-      .enum(["development", "test", "production"])
-      .default("development"),
-    NEXTAUTH_SECRET:
-      process.env.NODE_ENV === "production"
-        ? z.string()
-        : z.string().optional(),
-    NEXTAUTH_URL: z.preprocess(
+    NODE_ENV: z.enum(['development', 'test', 'production']).default('development'),
+    AUTH_JWT_SECRET: z.string(),
+    AUTH_JWT_EXPIRES_IN: z.string(),
+    AUTH_SECRET: process.env.NODE_ENV === 'production' ? z.string() : z.string().optional(),
+    AUTH_URL: z.preprocess(
       // This makes Vercel deployments not fail if you don't set NEXTAUTH_URL
       // Since NextAuth.js automatically uses the VERCEL_URL if present.
       (str) => process.env.VERCEL_URL ?? str,
       // VERCEL_URL doesn't include `https` so it cant be validated as a URL
-      process.env.VERCEL ? z.string() : z.string().url()
+      process.env.VERCEL ? z.string() : z.string().url(),
     ),
+    AUTH_GITHUB_ID: z.string(),
+    AUTH_GITHUB_SECRET: z.string(),
   },
   experimental__runtimeEnv: process.env,
   skipValidation: !!process.env.SKIP_ENV_VALIDATION,
