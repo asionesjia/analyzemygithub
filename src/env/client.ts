b639f29230ba6b71b4ef989f3174b052584b48c7
John Smith <asionesjia@gmail.com>
2024-10-26 02:39:57 +0800

    :bug: 更新envClient

--- src/env/client.ts
+++ src/env/client.ts
@@ -1,7 +1,11 @@
 import { createEnv } from '@t3-oss/env-nextjs'
+import { z } from 'zod'
 
 export const envClient = createEnv({
   client: {
+    NEXT_PUBLIC_APP_URL: z.string(),
   },
-  experimental__runtimeEnv: process.env
+  runtimeEnv: {
+    NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
+  },
 })
